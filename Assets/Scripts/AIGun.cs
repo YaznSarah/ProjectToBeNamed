cad5467fd85b78c48db76774e95005b6f58b6b9a
Yazn <36912068+YaznSarah@users.noreply.github.com>
2022-04-20 14:21:21 -0700

    attached weapon to new prefabs, should move accordingly with animation

--- Assets/Scripts/AIGun.cs
+++ Assets/Scripts/AIGun.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class AIGun : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject _weapon;
+    private AIManager _player;
+    private Vector3 _holsterLocation;
+    private Vector3 _holsterRotation;
+
+    void storePositions()
+    {
+        Vector3 initialLocation = _weapon.transform.localPosition;
+        Vector3 initialRotation = _weapon.transform.localEulerAngles;
+        _holsterLocation = initialLocation;
+        _holsterRotation = initialRotation;
+    }
+
     void Start()
     {
-        
+        _weapon = gameObject;
+        storePositions();
+        _player = GetComponentInParent<AIManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_player.isMoving)
+        {
+            _weapon.transform.localPosition = new Vector3(1.7581f, 1.4732f, 1.1357f);
+            _weapon.transform.localEulerAngles = new Vector3(2.147f, 15.318f, 2.474f);
+        }
+        else
+        {
+            _weapon.transform.localPosition = _holsterLocation;
+            _weapon.transform.localEulerAngles = _holsterRotation;
+        }
     }
 }
--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -8,7 +8,7 @@ public class AIManager : MonoBehaviour
     public float minDist = 10f;
     public float speed;
     private Animator _animator;
-    private bool _isMoving;
+    public bool isMoving;
 
 
     // Start is called before the first frame update
@@ -24,20 +24,20 @@ public class AIManager : MonoBehaviour
         Vector3 dist = (_player.transform.position - transform.position);
         if (dist.sqrMagnitude < minDist)
         {
-            _isMoving = true;
+            isMoving = true;
             transform.position =
                 Vector3.MoveTowards(transform.position, _player.transform.position, speed * Time.deltaTime);
-            _isMoving = true;
+            isMoving = true;
             transform.LookAt(_player.transform);
         }
         else
         {
-            _isMoving = false;
+            isMoving = false;
         }
 
-        if (_isMoving)
+        if (isMoving)
         {
-            _animator.SetBool("isMoving",_isMoving);
+            _animator.SetBool("isMoving",isMoving);
         }
     }
 }
